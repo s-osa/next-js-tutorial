cabe406fce1b6eaddb16bc2aca5b04ed0f360395
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2021-04-19 23:53:19 +0900

    Polish posts page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
+import Link from "next/link";
+import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from "../lib/posts";
+import {getSortedPostsData} from "../lib/posts";
+import Date from "../components/date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -12,7 +14,7 @@ export async function getStaticProps() {
   }
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
@@ -28,13 +30,18 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {allPostsData.map(({id, date, title}) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>
+                  {title}
+
+                </a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
